fc654e19a96fdec761ea583ecdc3e6e3cc5bb1a9
adam4056 <131684106+adam4056@users.noreply.github.com>
2024-01-24 20:06:41 +0100

    změna hl

--- server.js
+++ server.js
@@ -1,22 +1,45 @@
-// Import potřebných knihoven
 const express = require('express');
 const cors = require('cors');
+const https = require('https');
 
-// Vytvoření instance Express aplikace
 const app = express();
+const port = 3500;
 
-// Použití middleware pro povolení CORS
 app.use(cors());
 
-// Definice jednoduchého API endpointu
-app.get('/', (req, res) => {
-  res.json({ message: 'Ahoj z API!' });
+app.get('/api/hello', async (req, res) => {
+  try {
+    const options = {
+      hostname: 'www.dszo.cz',
+      path: '/online/tabs2.php',
+      method: 'GET',
+    };
+
+    const apiRequest = https.request(options, (apiResponse) => {
+      let data = '';
+
+      apiResponse.on('data', (chunk) => {
+        data += chunk;
+      });
+
+      apiResponse.on('end', () => {
+        // Zpracování odpovědi a odeslání klientovi
+        res.json({ message: JSON.parse(data) });
+      });
+    });
+
+    apiRequest.on('error', (error) => {
+      console.error('Chyba při volání API:', error.message);
+      res.status(500).json({ error: 'Interní chyba serveru' });
+    });
+
+    apiRequest.end();
+  } catch (error) {
+    console.error('Chyba:', error.message);
+    res.status(500).json({ error: 'Interní chyba serveru' });
+  }
 });
 
-// Nastavení portu, na kterém bude server poslouchat
-const port = 7000;
-
-// Spuštění serveru
 app.listen(port, () => {
   console.log(`Server běží na http://localhost:${port}`);
 });
